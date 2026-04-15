40f918525d1ae2ec5abd2d1e6499baf300c512a8
Mohammed112m <mh5205897@gmail.com>
2026-04-15 10:18:50 +0300

    Update

--- server/controllers/RideController.js
+++ server/controllers/RideController.js
@@ -1,25 +1,41 @@
-const Ride = require('../models/Ride')
+const Theme = require('../models/Theme')
 
-// GET
-const GetRides = async (req, res) => {
-  const rides = await Ride.find()
-  res.send(rides)
+// this is get  .. when need to show data
+const getThemes = async (req, res) => {
+  try {
+    const themes = await Theme.find()
+    res.json(themes)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 }
 
-// POST
-const CreateRide = async (req, res) => {
-  const ride = await Ride.create(req.body)
-  res.send(ride)
+
+// this is post   here create new data
+const createTheme = async (req, res) => {
+  try {
+    const newTheme = await Theme.create(req.body)
+    res.status(201).json(newTheme)
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
 }
 
-// DELETE
-const DeleteRide = async (req, res) => {
-  await Ride.findByIdAndDelete(req.params.id)
-  res.send({ msg: "Deleted" })
+//this is delete
+const deleteTheme = async (req, res) => {
+  try {
+    await Theme.findByIdAndDelete(req.params.id)
+    res.json({ message: "Theme deleted" })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 }
+
+
 
 module.exports = {
-  GetRides,
-  CreateRide,
-  DeleteRide
+  getThemes,
+  createTheme,
+  deleteTheme,
+  
 }
